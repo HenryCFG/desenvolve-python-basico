d800c67459e7abe10b15a6cfe5245a9ff83c3c6f
Henry <142840220+HenryCFG@users.noreply.github.com>
2025-12-06 17:29:25 -0300

    Create aula2_questao1.py

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,12 @@
+primeira_variavel = 5
+segunda_variavel = 2
+
+print("--- Tipos de Dados Iniciais ---")
+print(f"Tipo de primeira_variavel (5): {type(primeira_variavel)}")
+print(f"Tipo de segunda_variavel (2): {type(segunda_variavel)}")
+
+terceira_variavel = primeira_variavel / segunda_variavel
+
+print("--- Resultado da Divisão ---")
+print(f"Valor da terceira_variavel (5 / 2): {terceira_variavel}")
+print(f"Tipo de terceira_variavel: {type(terceira_variavel)}")
