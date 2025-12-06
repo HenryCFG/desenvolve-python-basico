97b187f749ebff41c2e3bfac2aef7b7b6c194a0f
Henry <142840220+HenryCFG@users.noreply.github.com>
2025-12-06 18:47:47 -0300

    Create aula1_questao5.py

---
+++ modulo5/aula1_questao5.py
@@ -0,0 +1,15 @@
+import emoji
+
+print("Emojis disponíveis:")
+print("❤️ - :red_heart:")
+print("👍 - :thumbs_up:")
+print("🤔 - :thinking_face:")
+print("🥳 - :partying_face:")
+print()
+
+frase_codificada = input("Digite uma frase e ela será emojizada: ")
+
+frase_emojizada = emoji.emojize(frase_codificada, language='en')
+
+print("Frase emojizada:")
+print(frase_emojizada)
